afbc9b0f75ad41ba3b44609e73fc2efeeaf6f1b5
offa <bm-dev@yandex.com>
2017-12-15 11:59:02 +0100

    Optional argument for passing the license file name.

--- license-header/add_license_header.sh
+++ license-header/add_license_header.sh
@@ -4,14 +4,17 @@ shopt -s globstar
 shopt -s extglob
 
 FILTER_ARG=${1}
+FILENAME_ARG=${2:-license_header}
+
 FILTER=${FILTER_ARG//,/\|}
 
-echo -e "Filter: '${FILTER_ARG}'\n"
+echo -e "Header: ${FILENAME_ARG}\nFilter: '${FILTER_ARG}'\n"
+
 
 
 for f in **/*.@(${FILTER})
 do
     echo " * Add license to <${f}>"
-    cat license_header ${f} > ${f}.tmp
+    cat ${FILENAME_ARG} ${f} > ${f}.tmp
     mv ${f}.tmp ${f}
 done
